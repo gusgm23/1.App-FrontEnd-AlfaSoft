29c7326740e7e90af922b53ef35e6b304305a9de
garySZA <garylvlsoliz@gmail.com>
2022-04-16 16:49:44 -0400

    Interfaz para registro de materia

--- src/components/Materias/RegistroMateria/RegistroMateria.js
+++ src/components/Materias/RegistroMateria/RegistroMateria.js
@@ -1,10 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from '../../../hooks/useForm';
 
 export const RegistroMateria = () => {
+    const [formValues, handleInputChange] = useForm({
+        codSis: '',
+        materia: '',
+        grupo: ''
+    });
+
+    const { codSis, materia, grupo } = formValues;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(formValues)
+
+    }
+    
     return (
-        <div>
-            <h2>RegistroMateria</h2>
-            <hr/>
+        <div className='contenedor-registro-aula'>
+            <h2 className='titulo-registro-aula'>Registro de Materia</h2>
+            <form onSubmit={ handleSubmit }>
+                <div className='contenedor-general'>
+                    <div className='contenedor-elementos'>
+                        <div className='contenedor-aula contenedor-flex'>
+                            <label className='labels'>Código SIS:</label>
+                            <input 
+                                name='codSis'
+                                className='inputs' 
+                                type='number'
+                                placeholder='201002903'
+                                value={codSis}
+                                onChange={ handleInputChange }
+                            />
+                        </div>
+                        <div className='contenedor-flex'>
+                            <label className='labels'>Materia:</label>
+                            <input 
+                                name='materia'
+                                className='inputs' 
+                                type='text'
+                                placeholder='Algebra II'
+                                value={ materia }
+                                onChange={ handleInputChange }
+                            />
+                        </div>
+                        <div className='contenedor-flex'>
+                            <label className='labels'>Grupo:</label>
+                            <input
+                                name='grupo'
+                                className='inputs' 
+                                type='text'
+                                placeholder='6A'
+                                value={ grupo }
+                                onChange={ handleInputChange }
+                            />
+                        </div>
+                    </div>
+                    <div className='contenedor-botones'>
+                        <button 
+                            type='button' 
+                            className='btn btn-warning'
+                            // onClick={handleCancel}
+                        >
+                            Cancelar
+                        </button>
+                        <button 
+                            type='submit' 
+                            className='btn btn-primary'
+                            // onClick={handleClick}
+                        >
+                            Guardar
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
