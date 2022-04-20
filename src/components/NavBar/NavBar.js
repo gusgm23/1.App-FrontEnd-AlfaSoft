f7423253727b0135a3364c8f158fd10f5cc88c98
garySZA <garylvlsoliz@gmail.com>
2022-04-19 22:31:27 -0400

    Corrección de errores en rutas

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,16 +11,11 @@ export const NavBar = () => {
                         <NavLink exact='true' activeClassName="active" to='/' className="nav-link" aria-current="page">Home</NavLink>
                         <NavLink exact='true' activeClassName="active" to='/registroaula' className="nav-link">Registrar Aula</NavLink>
                         <NavLink exact='true' activeClassName="active" to='/registrousuario' className="nav-link">Registrarse</NavLink>
-<<<<<<< HEAD:src/components/NavBar.js
-                        <NavLink exact='true' activeClassName="active" to='/login' className="nav-link">Login</NavLink>
                         <NavLink exact='true' activeClassName="active" to='/reservaaulas' className="nav-link">Reservar Aula</NavLink>
-=======
-                        <NavLink exact='true' activeClassName="active" to='/registromateria' className="nav-link">Registrar Materia</NavLink>
-
->>>>>>> ba480c3827075a15efee583405cb6830a510f48c:src/components/NavBar/NavBar.js
                         <NavLink exact='true' activeClassName="active" to='/veraulas' className="nav-link">Aulas</NavLink>
                         <NavLink exact='true' activeClassName="active" to='/vermaterias' className="nav-link">Materias</NavLink>
 
+                        
                         <NavLink exact='true' activeClassName="active" to='/login' className="nav-link">Login</NavLink>
                         <NavLink exact='true' activeClassName="active" to='/about' className="nav-link">About</NavLink>
                     </div>
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -6,32 +6,17 @@ import {
     Route,
 } from "react-router-dom";
 
-
-<<<<<<< HEAD:src/components/Aulas/routes/AppRouter.js
-import { NavBar } from '../../NavBar';
-import { AboutScreen } from '../../AboutScreen';
-import { HomeScreen } from '../../HomeScreen';
-import { LoginScreen } from '../../LoginScreen';
-
-//import { RegistroUsuarios } from './RegistroUsuariosScreen';
-
-import { RegistroUsuarios } from '../../RegistroUsuarios/RegistroUsuarios';
-import { Error404Screen } from '../../Page404/Error404Screen';
-import { VistaAulas } from '../../Aulas/Ver Aulas/VistaAulas';
-import { RegistroAulasScreen } from '../../Aulas/RegistroAulas/RegistroAulasScreen';
-import { FormularioReservaAula } from '../../ReservaAulas/FormularioReservaAula';
-=======
 import { NavBar } from '../components/NavBar/NavBar';
 import { AboutScreen } from '../components/AboutScreen';
 import { HomeScreen } from '../components/HomeScreen';
 import { LoginScreen } from '../components/LoginScreen';
-import { RegistroUsuarios } from '../components/RegistroUsuarios/RegistroUsuarios';
+import { RegistroUsuarios } from '../components/RegistroUsuariosScreen';
 import { Error404Screen } from '../components/Page404/Error404Screen';
 import { VistaAulas } from '../components/Aulas/Ver Aulas/VistaAulas';
 import { RegistroAulasScreen } from '../components/Aulas/RegistroAulas/RegistroAulasScreen';
+import { FormularioReservaAula } from '../components/ReservaAulas/FormularioReservaAula';
 import { RegistroMateria } from '../components/Materias/RegistroMateria/RegistroMateria';
 import { VerMaterias } from '../components/Materias/VerMaterias/VerMaterias';
->>>>>>> ba480c3827075a15efee583405cb6830a510f48c:src/routes/AppRouter.js
 
 
 export const AppRouter = () => {
@@ -50,12 +35,8 @@ export const AppRouter = () => {
                     <Route exact path='/registromateria'    element={<RegistroMateria/>}/>
 
                     <Route exact path='/veraulas'           element={<VistaAulas/>}/>
-<<<<<<< HEAD:src/components/Aulas/routes/AppRouter.js
-                    <Route exact path='/reservaaulas'        element={<FormularioReservaAula/>}/>
-=======
                     <Route exact path='/vermaterias'        element={<VerMaterias/>}/>
-
->>>>>>> ba480c3827075a15efee583405cb6830a510f48c:src/routes/AppRouter.js
+                    <Route exact path='/reservaaulas'        element={<FormularioReservaAula/>}/>
                     <Route path='*'                         element={<Error404Screen/>}/>
                 </Routes>
             </div>
