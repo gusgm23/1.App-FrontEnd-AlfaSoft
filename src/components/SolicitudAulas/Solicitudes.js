cf96b37f57253f440c3b5d78beb5749ce32dc93d
raquelcq <raqchipq@gmail.com>
2022-04-30 11:38:34 -0400

    solicitudes3

---
+++ src/components/SolicitudAulas/Solicitudes.js
@@ -0,0 +1,110 @@
+import React, { useState }  from 'react'
+import { NavLink } from 'react-router-dom'
+import { useModal } from '../../hooks/useModal';
+import { ModalGenerico } from '../Modal/ModalGenerico';
+import { RegSolicitud } from './RegistroSol/RegSolicitud';
+
+import './estilos-ver-soli.css'
+;
+
+export const Solicitudes = ({data=[]}) => {
+
+    const [values, setValues] = useState({
+        nombreDocenteSolicitud:'',
+        apellidoDocenteSolicitud:'',
+        numeroEstudiantesSolicitud: '',
+        motivoSolicitud:'',
+        fechaSolicitud:'',
+        horaInicioSolicitud:'',
+        periodoSolicitud:'',
+        estadoSolicitud:'',
+        materia_id:''
+
+    });
+
+    const { nombreDocenteSolicitud,apellidoDocenteSolicitud,numeroEstudiantesSolicitud,motivoSolicitud,fechaSolicitud,horaInicioSolicitud,periodoSolicitud,estadoSolicitud,materia_id} = values;
+    const [ isOpen, openModalEdicion, closeModalEdicion ] = useModal(false);
+
+    const actualizar = (item) => {
+        setValues({
+            nombreDocenteSolicitud: item.nombreDocenteSolicitud,
+            apellidoDocenteSolicitud: item.apellidoDocenteSolicitud,
+            numeroEstudiantesSolicitud: item.numeroEstudiantesSolicitud,
+            motivoSolicitud: item.motivoSolicitud,
+            fechaSolicitud: item.fechaSolicitud,
+            horaInicioSolicitud: item.horaInicioSolicitud,
+            periodoSolicitud: item.periodoSolicitud,
+            estadoDolicitud: item.estadoSolicitud,
+            materia_id: item.materia_id
+        });
+        openModalEdicion();
+    }
+    
+    const guardarID  = (id) => {
+        localStorage.setItem("id", id);
+    }
+
+    return (
+            <>
+            <div className='contenedor-tabla'>
+                <table>
+                    <thead>
+                        <tr className='titulo-tabla'>
+                            <th>#</th>
+                            <th>Nombre </th>
+                            <th>Apellido Docente</th>
+                            <th># de Estud.</th>
+                            <th>Motivo</th>
+                            <th>Fecha de Solicitud</th>
+                            <th>Hora de Solicitud</th>
+                            <th>Estado de Solicitud</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((item, i) => (
+                                <tr key={item.id}>
+                                    <td> { i+1 } </td>
+                                    <td> { item.nombreDocenteSolicitud } </td>
+                                    <td> { item.apellidoDocenteSolicitud } </td>
+                                    <td> { item.numeroEstudiantesSolicitud } </td>
+                                    <td> { item.motivoSolicitud } </td>
+                                    <td> { item.fechaSolicitud } </td>
+                                    <td> { item.horaInicioSolicitud } </td>
+                                    <td> { item.estadoSolicitud } </td>
+                                    <td className='td-btns'>
+                                        <section className='caja-btns'>
+                                            <button 
+                                                className='btn-editar editar-mat'
+                                                onClick={ () => {actualizar(item)} }
+                                            >
+                                                Detalles
+                                            </button>
+                                        </section>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {
+                isOpen &&
+                <ModalGenerico isOpen={ isOpen } closeModal={closeModalEdicion}>
+                    <RegSolicitud nombre_doc ={nombreDocenteSolicitud} 
+                    ape_doc ={apellidoDocenteSolicitud} 
+                    nro_est ={numeroEstudiantesSolicitud} 
+                    motivo ={motivoSolicitud}
+                    fecha_res ={fechaSolicitud}
+                    hora_res ={horaInicioSolicitud}
+                    periodo ={periodoSolicitud}
+                    estado ={estadoSolicitud}
+                    closeModal={closeModalEdicion} 
+                    titulo='Detalles' 
+                    materia_id={materia_id} />
+                </ModalGenerico>
+            }
+            </>
+    )
+}
--- src/service/apiSolicitudAulas.js
+++ src/service/apiSolicitudAulas.js
@@ -1,65 +1,71 @@
 import axios from "axios";
 
-export const getSolicitud = () => {
-    return axios.get(`http://127.0.0.1:8000/api/obtenerSolicitud`);
+export const getSolicitud = (setListaSolicitud) => {
+    return axios.get(`http://127.0.0.1:8000/api/obtenerSolicitud`)
+        .then(response => {
+            setListaSolicitud({
+                state: true,
+                data: response.data
+            });
+        })
+        .catch(e => {
+            console.log(e);
+        })
+
 }
 
 export const getSolicitudId = (id) => {
     return axios.get(`http://127.0.0.1:8000/api/obtenerSolicitudId/${id}`);
 }
 
-export const createSolicitud = ( 
-    {   nombreDocente, 
-        apellidoDocente, 
-        cantidadEstudiantes, 
-        motivoSolicitud, 
-        fechaSolicitud, 
-        horaSolicitud, 
-        peridoSolicitud, 
-    }, materia_id, pendiente, openModalSuccess, openModalWarning,) => {
-    return axios.post(`http://127.0.0.1:8000/api/crearSolicitud`, 
-    {
+export const createSolicitud = ({
+    nombreDocente,
+    apellidoDocente,
+    cantidadEstudiantes,
+    motivoSolicitud,
+    fechaSolicitud,
+    horaSolicitud,
+    peridoSolicitud,
+}, materia_id, pendiente, openModalSuccess, openModalWarning, ) => {
+    return axios.post(`http://127.0.0.1:8000/api/crearSolicitud`, {
         //id:                             `${data.id}`,
-        nombreDocenteSolicitud:         `${nombreDocente}`,
-        apellidoDocenteSolicitud:       `${apellidoDocente}`,
-        numeroEstudiantesSolicitud:     `${cantidadEstudiantes}`,
-        motivoSolicitud:                `${motivoSolicitud}`,
-        fechaSolicitud:                 `${fechaSolicitud}`,
-        horaInicioSolicitud:            `${horaSolicitud}`,
+        nombreDocenteSolicitud: `${nombreDocente}`,
+        apellidoDocenteSolicitud: `${apellidoDocente}`,
+        numeroEstudiantesSolicitud: `${cantidadEstudiantes}`,
+        motivoSolicitud: `${motivoSolicitud}`,
+        fechaSolicitud: `${fechaSolicitud}`,
+        horaInicioSolicitud: `${horaSolicitud}`,
         //horaFinSolicitud:               `${}`,
-        periodoSolicitud:               `${peridoSolicitud}`,
-        estadoSolicitud:                `${pendiente}`,
-        materia_id:                     `${materia_id}`
-        //nombreMateria:                  `${data.nombreMateria}`,
-        //grupoMateria:                   `${data.grupoMateria}`
-    }
-    ).then( (response) => {
+        periodoSolicitud: `${peridoSolicitud}`,
+        estadoSolicitud: `${pendiente}`,
+        materia_id: `${materia_id}`
+            //nombreMateria:                  `${data.nombreMateria}`,
+            //grupoMateria:                   `${data.grupoMateria}`
+    }).then((response) => {
         openModalSuccess();
-    }).catch( (error) => {
+    }).catch((error) => {
         openModalWarning();
     });
 }
 
 export const updateSolicitudId = (data, id) => {
-    return axios.put(`http://127.0.0.1:8000/api/actualizarSolicitud/${id}`,
-    {
-        id:                             `${data.id}`,
-        nombreDocenteSolicitud:         `${data.nombreDocenteSolicitud}`,
-        apellidoDocenteSolicitud:       `${data.apellidoDocenteSolicitud}`,
-        numeroEstudiantesSolicitud:     `${data.numeroEstudiantesSolicitud}`,
-        motivoSolicitud:                `${data.motivoSolicitud}`,
-        fechaSolicitud:                 `${data.fechaSolicitud}`,
-        horaInicioSolicitud:            `${data.horaInicioSolicitud}`,
-        horaFinSolicitud:               `${data.horaFinSolicitud}`,
-        periodoSolicitud:               `${data.periodoSolicitud}`,
-        estadoSolicitud:                `${data.estadoSolicitud}`,
-        materia_id:                     `${data.materia_id}`
-        //nombreMateria:                  `${data.nombreMateria}`,
-        //grupoMateria:                   `${data.grupoMateria}`
-    }
-    ).then(function (response) {
+    return axios.put(`http://127.0.0.1:8000/api/actualizarSolicitud/${id}`, {
+        id: `${data.id}`,
+        nombreDocenteSolicitud: `${data.nombreDocenteSolicitud}`,
+        apellidoDocenteSolicitud: `${data.apellidoDocenteSolicitud}`,
+        numeroEstudiantesSolicitud: `${data.numeroEstudiantesSolicitud}`,
+        motivoSolicitud: `${data.motivoSolicitud}`,
+        fechaSolicitud: `${data.fechaSolicitud}`,
+        horaInicioSolicitud: `${data.horaInicioSolicitud}`,
+        horaFinSolicitud: `${data.horaFinSolicitud}`,
+        periodoSolicitud: `${data.periodoSolicitud}`,
+        estadoSolicitud: `${data.estadoSolicitud}`,
+        materia_id: `${data.materia_id}`
+            //nombreMateria:                  `${data.nombreMateria}`,
+            //grupoMateria:                   `${data.grupoMateria}`
+    }).then(function(response) {
         console.log(response);
-    }).catch(function (error) {
+    }).catch(function(error) {
         console.log(error);
     });
 }
