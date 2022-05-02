1de907a9d81e545b606ca8d09874b0c67da4d539
raquelcq <raqchipq@gmail.com>
2022-05-02 17:41:03 -0400

    solicitudes5

--- src/components/SolicitudAulas/VerSolicitudes.js
+++ src/components/SolicitudAulas/VerSolicitudes.js
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react'
 import { getSolicitud } from '../../service/apiSolicitudAulas'
 import Spinner from '../Spinner/Spinner'
 import { Solicitudes } from './Solicitudes'
+import { useSortTable } from '../../hooks/usesort'
 //import ListaSolicitudes from '../../data/ListaSolicitudes'
 
 import './estilos-ver-soli.css'
 
 export const VerSolicitudes = () => {
-    const [ListaSolicitud, setListaSolicitud] = useState({
+    const [ListaSolicitud, setListaSolicitud, sort] = useSortTable(useState({
         state: false,
         data: []
-    });
+    }),
+    'fechaSolicitud'
+    )
 
     const {state, data} = ListaSolicitud;
 
@@ -21,7 +24,9 @@ export const VerSolicitudes = () => {
     return (
         <div className='contenedor-gral'>
             <div className='contenedor-elementos-lista'>
-                <h2 className='titulo-ver-aulas' >Solicitudes Registradas </h2>
+                <h2 className='titulo-ver-soli' >Solicitudes Registradas </h2>
+                
+
                 <hr/>
                 {
                     state ?
@@ -32,51 +37,4 @@ export const VerSolicitudes = () => {
 
         </div>
     )
-
-/*
-    return (
-        <div class="container-lg">
-            <div class="row justify-content-md-center">
-                    <div class="row row-cols-1">
-                        <div class="col-center">
-                            <div class="Titulo-Soli">
-                                Lista de Solicitudes
-                                </div>
-                            </div>
-                        <div class="col-center">
-                            <div class="row justify-content-xl-center">
-                                <div class="col col-auto-center">
-                                    <div class="titulos-soli">
-                                        Materia
-                                    </div>
-                                </div>
-                                <div class="col col-auto-center">
-                                    <div class="titulos-soli">
-                                        Aula
-                                    </div>
-                                </div>
-                                <div class="col col-auto-center">
-                                    <div class="titulos-soli">
-                                        Fecha de Solicitud
-                                    </div>
-                                </div>
-                                <div class="col col-auto-center">
-                                    <div class="titulos-soli">
-                                        Detalle
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-center">
-                            {
-                                ListaSolicitudes.map( (item) => (
-                                    <Solicitudes elem={item}/>
-                                ) )
-                            }
-                        </div>
-                    </div>
-            </div>
-
-        </div>
-    )*/
 }
