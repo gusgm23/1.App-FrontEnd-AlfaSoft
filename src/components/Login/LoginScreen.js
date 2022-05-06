ba3796512df47f939e89c7b2288ff873f16e5b3c
garySZA <garylvlsoliz@gmail.com>
2022-05-05 22:01:21 -0400

    Maquetación de interfaz del login

---
+++ src/components/Login/LoginScreen.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './estilos-login.css'
+
+export const LoginScreen = () => {
+    
+    const handleLogin = () => {
+        
+        console.log('*se deberia logear*')
+
+    }
+    
+    return (
+        <div className='container-flex-login'>
+            <div className='container-elements-login'>
+                <h2 className='title-login'>Iniciar Sesión</h2>
+                <div className='container-inputs-login'>
+                    <label className='label-login element-login'>Correo Institucional:</label>
+                    <input 
+                        type='email'
+                        placeholder='2020028394@fcyt.umss.edu.bo'
+                        className='element-login'
+                    />
+                    <label className='label-login element-login'>Contraseña:</label>
+                    <input 
+                        type='password'
+                        placeholder='***********'
+                        className='element-login'
+                        autoComplete='off'
+                    />
+                    <Link to='/' className='link element-login'>¿No tienes cuenta? Registrate aqui</Link>
+                </div>
+                <section>
+                    <button 
+                        className='btn-login'
+                        onClick={ handleLogin }
+                    >
+                        Ingresar
+                    </button>
+                    <Link to='/' className='link element-login'>Volver</Link>
+                </section>
+            </div>
+        </div>
+    )
+}
