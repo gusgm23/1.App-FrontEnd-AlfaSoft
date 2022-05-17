58e077df07ecfabc46fde637ab08ccd0f2085849
garySZA <garylvlsoliz@gmail.com>
2022-05-16 22:43:31 -0400

    correccion de errores

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -7,7 +7,7 @@ import './estilos-home.css'
 export const HomeScreen = () => {
     return (
         <>
-            <h2 className='titulo-home animate__animated animate__fadeIn'>Sistema de Asignación de Aulas</h2>
+            <h2 className='titulo-home animate__animated animate__fadeIn'>Sistema de Reserva de Aulas</h2>
             <hr/>
             <div className='contenido-home'>
                 <p className='parrafo-home animate__animated animate__fadeIn'>El sistema de asignación de aulas es un sistema que tiene objetivo de poder ayudar a los docentes como también a los auxiliares de la facultad de Ciencias y Tecnología de la universidad Mayor de San Simón a poder realizar la reserva de aulas para que puedan tomar evaluaciones.</p>
