6a55d3cb51c52b93ddc627465d5def64542d258f
gusgm33 <gustavogonzalesmosquera@gmail.com>
2022-06-07 00:30:57 -0400

    Deploy en heroku

--- src/helpers/actualizarCapacidadSolicitud.js
+++ src/helpers/actualizarCapacidadSolicitud.js
@@ -2,13 +2,24 @@ import { updateSolicitud } from "../service/apiSolicitudAulas"
 
 //!Metodo encargado de reducir la capacidad de una solicitud en la bd
 export const reducirCapacidadSolicitud = (solicitud, nuevaCantidad) => {
+    if( nuevaCantidad > 0){
+        
+        const nuevaSolicitud = {
+            ...solicitud,
+            numeroEstudiantesSolicitud: nuevaCantidad
+    
+        }
+    
+        updateSolicitud(nuevaSolicitud);
 
-    const nuevaSolicitud = {
-        ...solicitud,
-        numeroEstudiantesSolicitud: nuevaCantidad
-
+    }else{
+        const nuevaSolicitud = {
+            ...solicitud,
+            numeroEstudiantesSolicitud: 0,
+            estadoSolicitud: 'Aprobado'
+        }
+    
+        updateSolicitud(nuevaSolicitud);
     }
-
-    updateSolicitud(nuevaSolicitud);
 
 }
